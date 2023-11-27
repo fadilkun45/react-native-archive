27c86b53ff67b2b9bad7ae71f0a664792523c3b6
fadilkun45 <indocrafter76@gmail.com>
2023-11-28 06:21:50 +0700

    dev

--- Screens/Detail.js
+++ Screens/Detail.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Image } from 'react-native';
 import { SafeAreaView } from 'react-native';
 import { Text } from 'react-native-paper';
+import { customFormatNumber } from '../lib/Helpers';
 
 const Detail = ({route,navigation}) => {
 
@@ -11,7 +12,7 @@ const Detail = ({route,navigation}) => {
     <SafeAreaView style={{ width: "100%", display: "flex", marginTop: 10, alignItems: "center" }}>
     <Text variant='headlineLarge' style={{textAlign: "center"}}>{params?.name?.common}</Text>
     <Image source={{uri: params?.flags?.png}}  style={{width: 300, height: 150, marginTop: 20}} />
-    <Text variant='bodyLarge' style={{marginTop: 20}}>Populasi: {params?.population}</Text>
+    <Text variant='bodyLarge' style={{marginTop: 20}}>Populasi: {customFormatNumber(params?.population)}</Text>
     <Text variant='bodyLarge' style={{marginTop: 20}}>Ibukota: {params?.name?.common === "Palestine" ? params?.capital[1] :  params?.capital[0]}</Text>
     <Text variant='bodyLarge' style={{marginTop: 20}}>benua: {params?.continents[0]}</Text>
     </SafeAreaView>
--- Screens/Home.js
+++ Screens/Home.js
@@ -4,9 +4,6 @@ import { SafeAreaView } from 'react-native-safe-area-context'
 import { Button, Card, MD3Colors, Modal, PaperProvider, Portal, ProgressBar, Provider, Searchbar, Text } from 'react-native-paper'
 import { fetchAll } from '../services/fetchData'
 import { FlatList } from 'react-native'
-import { ScrollView } from 'react-native'
-import { Image } from 'react-native'
-import deepClone from 'clonedeep'
 
 const Home = ({navigation}) => {
     const [search, setSearch] = useState('')
@@ -47,34 +44,31 @@ const Home = ({navigation}) => {
 
             <PaperProvider>
                 <Portal>
-                    <Modal visible={modal} onDismiss={() => setModal(false)} contentContainerStyle={containerStyle}>
-                        <Text style={{  textAlign: "center", paddingVertical: 20 }} variant='titleLarge' >
+                    <Modal  visible={modal} onDismiss={() => setModal(false)} contentContainerStyle={containerStyle}>
+                        <Text style={{  textAlign: "center", paddingVertical: 20, color: "#303030" }} variant='titleLarge' >
                         {`Negara tidak ditemukan, maksud mu palestina ? \n\n\n\n  #Freepalestine\n#ceasefirenow`}
                         </Text>
                     </Modal>
                 </Portal>
-                {/* <Button style={{ marginTop: 30 }} onPress={showModal}>
-                    Show
-                </Button> */}
-                <SafeAreaView style={{ width: "100%", display: "flex", alignItems: "center" }}>
+                <SafeAreaView style={{ width: "100%", display: "flex", alignItems: "center", color: "#303030" }}>
                     <View style={{ width: "90%", }}>
-                        <Text variant="titleLarge" style={{ textAlign: "center", display: "flex", marginVertical: 13 }}>List Negara</Text>
+                        <Text variant="titleLarge" style={{ textAlign: "center", display: "flex", marginVertical: 13,color: "#303030" }}>List Negara</Text>
                         <Searchbar
-                            style={{ backgroundColor: "white" }}
+                            style={{ backgroundColor: "white", color: "#303030"  }}
                             placeholder="Search"
                             onChangeText={onChangeSearch}
                             value={search}
                         />
 
                         {
-                            newData?.length === 0 ? <Text style={{ textAlign: "center", marginTop: 40 }} variant='displayMedium'>Data tidak ditemukan</Text> : <FlatList
-                                style={{ marginTop: 14, marginBottom: 30 }}
+                            newData?.length === 0 ? <Text style={{ textAlign: "center", marginTop: 40,  color: "#303030"  }}  variant='displayMedium'>Data tidak ditemukan</Text> : <FlatList
+                                style={{ marginTop: 14, marginBottom: 300, display: "flex" }}
                                 data={newData}
                                 removeClippedSubviews
                                 keyExtractor={({ item, index }) => index}
                                 renderItem={({ item, index }) => <Card style={{ marginBottom: 23, backgroundColor: "white" }} onPress={() => onPressDetail(item)}>
-                                    <Card.Title title={item?.name?.common} />
-                                    <Card.Cover style={{padding: 10}} source={{ uri: item?.flags?.png }} />
+                                    <Card.Title title={item?.name?.common} titleVariant="headlineSmall" titleStyle={{ color: "#303030" }} />
+                                    <Card.Cover style={{padding: 10,  color: "#303030" }} source={{ uri: item?.flags?.png }} />
                                 </Card>
                                 }
                             />
